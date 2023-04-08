f279be938ea6a38d2a7ce3422deb26096cdae53a
Youngho-kr <yyoung0997@gmail.com>
2023-04-08 02:46:17 -0700

    feat: phase3

--- phase1/shellex.c
+++ phase1/shellex.c
@@ -26,10 +26,391 @@ int checkPipe(int bg, char **argv, char *cmdline);
 
 void call_pipe(int bg, char **argv, char *cmdline, int pipe_index);
 
+void sigint_handler(int sg);
+void sigchld_handler(int sg);
+void sigtstp_handler(int sg);
+
+void printJobs();
+void printJob(int pid);
+
+/*
+num : index
+pid : pidnumber
+status : 0 -> suspended
+         1 -> running
+processName : processname
+next : next list
+*/
+typedef struct process
+{
+    int num;
+    int pid;
+    int status;
+    char processName[FILENAME_MAX];
+    struct process *next;
+} process;
+
+/*
+num = 0
+next = NULL
+*/
+process *bg_process;
+
+void addProcess(process *new_process);
+void deleteProcess(process *prev, process *cur);
+
+void foreground(char **argv);
+void background(char **argv);
+void killProcessIndex(char **argv);
+void killProcessPID(int pid);
+
 char HISTORY_PATH[MAXLINE];
+
+volatile pid_t pid;
+int main_pid;
+int fg_pid;
+char fg_processName[MAXLINE];
+
+/*
+    new_process를 process linked_list 마지막에 추가
+*/
+void addProcess(process *new_process) {
+    int index;
+    process *tmp_process = bg_process;
+    while(1) {
+        index = tmp_process -> num;
+        if(tmp_process -> next == NULL)
+            break;
+        tmp_process = tmp_process -> next;
+    }
+
+    new_process->num = index + 1;
+    tmp_process -> next = new_process;
+}
+
+/* cur process를 linked list에서 삭제 */
+void deleteProcess(process *prev, process *cur)
+{
+    prev->next = cur->next;
+    free(cur);
+}
+
+void sigtstp_handler(int sig)
+{
+    if(main_pid == getpid()){
+        if(fg_pid != main_pid) {
+            process *tmp_process = bg_process;
+            tmp_process = tmp_process -> next;
+            int flag = 0;
+            while(1) {
+                if(tmp_process == NULL)
+                    break;
+                if(tmp_process -> pid == fg_pid){
+                    flag = 1;
+                    break;
+                }
+                tmp_process = tmp_process -> next;
+            }
+
+            if(flag == 0){
+
+            process *new_process = (process *)malloc(sizeof(process));
+            new_process->pid = fg_pid;
+            strcpy(new_process->processName, fg_processName);
+            new_process->status = 0;
+            new_process->next = NULL;
+
+            addProcess(new_process);
+            }
+            else {
+                tmp_process->status = 0;
+            }
+            
+            kill(fg_pid, SIGTSTP);
+        }
+    }
+}
+
+void sigint_handler(int sig)
+{
+    // ctrl + c handler
+    if (main_pid == getpid())
+    {
+        if(fg_pid != main_pid) {
+            int index;
+            process *tmp_process = bg_process;
+            tmp_process = tmp_process -> next;
+            process *prev_process = bg_process;
+
+            while(1) {
+                if(tmp_process == NULL)
+                    break;
+
+                if(tmp_process->pid == fg_pid){
+                    deleteProcess(prev_process, tmp_process);
+                }
+
+                prev_process = tmp_process;
+                tmp_process = tmp_process -> next;
+            }
+
+            kill(fg_pid, SIGKILL);
+        }
+    }
+}
+
+void sigchld_handler(int sig)
+{
+    // int olderrno = errno;
+    pid_t pid_chld;
+    int status;
+    if(main_pid == getpid()) {
+    while ((pid_chld = waitpid(-1, &status, WNOHANG)) > 0)
+    {
+        int index = 0;
+        process *tmp_process = bg_process;
+        tmp_process = tmp_process -> next;
+        process *prev_process = bg_process;
+        while(1){
+            if(tmp_process == NULL)
+                break;
+
+            if(tmp_process -> pid == pid_chld) {
+                deleteProcess(prev_process, tmp_process);
+            }
+
+            prev_process = tmp_process;
+            tmp_process = tmp_process -> next;
+        }
+        //killProcessPID(pid_chld);
+
+        //kill(pid_chld, SIGTERM);
+        // sio_puts("Handler repead child ");
+        // sio_putl((long)pid_chld);
+        // sio_puts("\n");
+
+    }
+    // if (errno != ECHILD)
+    //     sio_error("wait error");
+    // errno = olderrno;
+    }
+}
+
+int parseBackground(char *num)
+{
+    if (num == NULL)
+        return -2;
+    if (num[0] != '%')
+        return -1;
+    else
+    {
+        if (num[1] == '\0')
+            return -1;
+        int index = atoi(&(num[1]));
+        return index;
+    }
+}
+
+void foreground(char **argv) {
+    int index = parseBackground(argv[1]);
+
+    int flag = 0;
+    process *tmp_process;
+    if(index > 0) {
+        tmp_process = bg_process;
+        tmp_process = tmp_process -> next;
+
+        while(1) {
+            if(tmp_process == NULL)
+                break;
+
+            if(tmp_process->num == index) {
+                flag = 1;
+                break;
+            }
+
+            tmp_process = tmp_process->next;
+        }
+    }
+
+    if(flag == 1) {
+        tmp_process->status = 1;
+        kill(tmp_process->pid, SIGCONT);
+
+        int status;
+        if(waitpid(pid, &status, WUNTRACED) < 0) {
+            unix_error("waitfg: waitpid error" );
+        }
+    }
+    else {
+        printf("No Such Job\n");
+    }
+}
+void background(char **argv) {
+    int index = parseBackground(argv[1]);
+
+    int flag = 0;
+    process *tmp_process;
+    if(index > 0) {
+        tmp_process = bg_process;
+        tmp_process = tmp_process -> next;
+
+        while(1) {
+            if(tmp_process == NULL)
+                break;
+
+            if(tmp_process->num == index) {
+                flag = 1;
+                break;
+            }
+
+            tmp_process = tmp_process->next;
+        }
+    }
+
+    if(flag == 1) {
+        tmp_process->status = 1;
+        kill(tmp_process->pid, SIGCONT);
+    }
+    else {
+        printf("No Such Job\n");
+    }
+}
+
+void killProcessIndex(char **argv)
+{
+    // kill(main_pid, SIGTERM);
+    int index = parseBackground(argv[1]);
+
+    if (index <= 0)
+    {
+        printf("No Such Job\n");
+    }
+
+    printf("index: %d\n", index);
+
+    int flag = 0;
+    process *tmp_process = bg_process;
+    process *prev_process = bg_process;
+
+    tmp_process = tmp_process->next;
+    while (1)
+    {
+        if(tmp_process == NULL)             // linked list 끝에 도달
+            break;
+        if(tmp_process->num >index)         // num 없음
+            break;
+
+        if (tmp_process->num == index)
+        {
+            flag = 1;
+            break;
+        }
+
+        prev_process = tmp_process;
+        tmp_process = tmp_process->num;
+    }
+
+    if (flag == 1)
+    {
+        printf("killprocess: %d\n", tmp_process->pid);
+        kill(tmp_process->pid, SIGKILL);
+        waitpid(tmp_process->pid, 0, WUNTRACED);
+
+        deleteProcess(prev_process, tmp_process);
+    }
+    else
+    {
+        printf("No Such Job\n");
+    }
+}
+
+void killProcessPID(int pid) {
+    process *tmp_process = bg_process;
+    tmp_process = tmp_process -> next;
+    process *prev_process = bg_process;
+
+    while(1) {
+        if(tmp_process == NULL)
+            break;
+        if(tmp_process->pid == pid) {
+            kill(tmp_process->pid, SIGKILL);
+            waitpid(tmp_process->pid, 0, 0);
+
+            deleteProcess(prev_process, tmp_process);
+        }
+
+        prev_process = tmp_process;
+        tmp_process = tmp_process->num;
+    }
+}
+
+void printJobs()
+{
+    process *tmp_process = bg_process;
+    tmp_process = tmp_process->next;
+
+    while (1)
+    {
+        if (tmp_process == NULL)
+            break;
+
+        if (tmp_process->status == 0)
+        {
+            printf("[%d] suspended %s\n", tmp_process->num, tmp_process->processName);
+        }
+        if (tmp_process->status == 1)
+        {
+            printf("[%d] running %s\n", tmp_process->num, tmp_process->processName);
+        }
+
+        tmp_process = tmp_process->next;
+    }
+}
+
+void printJob(int pid)
+{
+    process *tmp_process = bg_process;
+
+    while (tmp_process->next != NULL)
+    {
+        if (tmp_process->pid == pid)
+            break;
+
+        tmp_process = tmp_process->next;
+    }
+
+    printf("[%d] %d\n", tmp_process->num, tmp_process->pid);
+}
+
+void writeJob(int pid, char *cmdline)
+{
+    process *tmp_process = bg_process;
+
+    int index = 0;
+    while (1)
+    {
+        if (tmp_process->next == NULL)
+            break;
+        index = tmp_process->num;
+    }
+
+    process *new_process = (process *)malloc(sizeof(process));
+    new_process->num = index + 1;
+    new_process->pid = pid;
+    strcpy(new_process->processName, cmdline);
+    new_process->status = 1;
+    new_process->next = NULL;
+
+    tmp_process->next = new_process;
+}
 
 int main()
 {
+    // main process의 pid num
+    main_pid = getpid();
+    fg_pid = getpid();
+
     // HISTORY_PATH에 history경로 저장
     if (getcwd(HISTORY_PATH, MAXLINE) == NULL)
     {
@@ -37,11 +418,28 @@ int main()
         exit(1);
     }
 
-    strcat(HISTORY_PATH, "/history");
+    strcat(HISTORY_PATH, "/.history");
 
     //  history 파일 생성
     FILE *file = fopen(HISTORY_PATH, "a");
     fclose(file);
+
+    // SIGNAL
+    // SIGINT, SIGCHLD, SIGTSTP handler
+    sigset_t mask, prev_mask;
+
+    if (signal(SIGINT, sigint_handler) == SIG_ERR)
+        perror("signal error");
+
+    if (signal(SIGCHLD, sigchld_handler) == SIG_ERR)
+        perror("signal error");
+
+    if (signal(SIGTSTP, sigtstp_handler) == SIG_ERR)
+        perror("signal error");
+
+    bg_process = (process *)malloc(sizeof(process));
+    bg_process->next = NULL;
+    bg_process->num = 0;
 
     char cmdline[MAXLINE]; /* Command line */
 
@@ -56,6 +454,8 @@ int main()
         /* Evaluate */
         eval(cmdline);
     }
+
+    free(bg_process);
 }
 /* $end shellmain */
 
@@ -66,7 +466,7 @@ void eval(char *cmdline)
     char *argv[MAXARGS]; /* Argument list execve() */
     char buf[MAXLINE];   /* Holds modified command line */
     int bg;              /* Should the job run in bg or fg? */
-    pid_t pid;           /* Process id */
+    // pid_t pid;           /* Process id */
 
     removeEnter(cmdline);
 
@@ -87,8 +487,25 @@ void eval(char *cmdline)
 /* If first arg is a builtin command, run it and return true */
 int builtin_command(char **argv)
 {
-    if (!strcmp(argv[0], "exit")) /* quit command */
+    if (!strcmp(argv[0], "exit")) { /* quit command */
+        // free all process
+        process* cur = bg_process;
+        cur = cur->next;
+        process* prev = bg_process;
+
+        while(1) {
+            if(cur == NULL)
+                break;
+            kill(cur->pid, SIGKILL);
+
+            free(prev);
+            prev = cur;
+            cur = cur->next;
+        }
+        free(prev);
+        
         exit(0);
+    }
     if (!strcmp(argv[0], "&")) /* Ignore singleton & */
         return 1;
     if (!strcmp(argv[0], "cd"))
@@ -101,10 +518,29 @@ int builtin_command(char **argv)
         openHistory(-1);
         return 1;
     }
+    if (!strcmp(argv[0], "jobs"))
+    {
+        printJobs();
+        return 1;
+    }
+    if (!strcmp(argv[0], "fg"))
+    {
+        foreground(argv);
+        return 1;
+    }
+    if (!strcmp(argv[0], "bg"))
+    {
+        background(argv);
+        return 1;
+    }
+    if (!strcmp(argv[0], "kill"))
+    {
+        killProcessIndex(argv);
+        return 1;
+    }
     return 0; /* Not a builtin command */
 }
 /* $end eval */
-
 /* $begin parseline */
 /* parseline - Parse the command line and build the argv array */
 int parseline(char *buf, char **argv)
@@ -212,7 +648,8 @@ void removeEnter(char *cmdline)
 
 void checkBuiltin(int bg, char **argv, char *cmdline)
 {
-    pid_t pid;
+    // pid_t pid;
+    strcpy(fg_processName, cmdline);
 
     if (!builtin_command(argv))
     { // quit -> exit(0), & -> ignore, other -> run
@@ -220,8 +657,7 @@ void checkBuiltin(int bg, char **argv, char *cmdline)
         // pipe가 없는 경우 이 함수에서 처리
 
         int pipe_index = checkPipe(bg, argv, cmdline); // argv[] = "|"인 가장 가까운 index, pipe_index = -1 --> '|' 존재하지않음
-        // pipe가 존재할 때
-        if (pipe_index > 0)
+        if (pipe_index > 0)                            // pipe가 존재
         {
             if ((pid = fork()) == 0)
             {
@@ -230,32 +666,48 @@ void checkBuiltin(int bg, char **argv, char *cmdline)
                 exit(0);
             }
         }
-        else if (pipe_index == 0)
+        else if (pipe_index == 0) // argv[0] = "|"
         {
             printf("bash: syntax error near unexpected token `\\'\n");
             return;
         }
-        else if (pipe_index = -1)
+        else if (pipe_index = -1) // pipe 존재하지 않음
         {
             if ((pid = fork()) == 0)
             {
+                if (!bg)
+                {
+                }
+                else
+                {
+                    //signal(SIGINT, SIG_IGN);
+                    //signal(SIGTSTP, SIG_IGN);
+                    // ignore sigint, sigstsp
+                }
+
                 if (execvpe(argv[0], argv, environ) < 0)
                 { // ex) /bin/ls ls -al &
                     printf("%s: Command not found.\n", argv[0]);
-                    exit(0);
                 }
+                exit(0);
             }
         }
 
         /* Parent waits for foreground job to terminate */
         if (!bg)
         {
+            fg_pid = pid;
             int status;
-            if (waitpid(pid, &status, 0) < 0)
-                unix_error("waitfg: waitpid error");
+            if(waitpid(pid, &status, WUNTRACED) < 0) {
+                unix_error("waitfg: waitpid error" );
+            }
         }
-        else // when there is backgrount process!
-            printf("%d %s", pid, cmdline);
+        else
+        { // when there is backgrount process!
+            // printf("[%d] %s\n", pid, cmdline);
+            writeJob(pid, cmdline);
+            printJob(pid);
+        }
     }
 }
 
@@ -335,7 +787,7 @@ void call_pipe(int bg, char **argv, char *cmdline, int pipe_index)
     new_argv[j] = NULL;
     argv[pipe_index] = NULL;
 
-    pid_t pid;
+    // pid_t pid;
     int fds[2];
     pipe(fds);
 
@@ -346,13 +798,14 @@ void call_pipe(int bg, char **argv, char *cmdline, int pipe_index)
         dup2(fds[1], STDOUT_FILENO);
         close(fds[1]);
 
-        execvp(argv[0], argv);
+        // execvp(argv[0], argv);
+
+        checkBuiltin(bg, argv, cmdline);
 
         exit(0);
     }
     else
     {
-
         close(fds[1]);
         dup2(fds[0], STDIN_FILENO);
         close(fds[0]);
@@ -361,7 +814,7 @@ void call_pipe(int bg, char **argv, char *cmdline, int pipe_index)
 
         pipe_index = checkPipe(bg, new_argv, cmdline);
 
-        if(pipe_index == - 1)
+        if (pipe_index == -1)
             execvp(new_argv[0], new_argv);
         else
             call_pipe(bg, new_argv, cmdline, checkPipe(bg, new_argv, cmdline));
